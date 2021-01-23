4c3261f5a86e879ebc3bb3e7848fa2f90390adb6
kamira721 <kamiravelasquez@gmail.com>
2021-01-23 19:43:54 -0300

    subiendo trabajo

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
+// Función de jQuery para que se activen los popover de los botones que tengan el atributo data-toggle con el valor popover//
